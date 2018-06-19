fdae7f93b53cbd4c1d80a4197825580b32579737
SnowySailor <kanel.nick@gmail.com>
2018-06-18 22:35:10 -0400

    Refactor FromJSON parsers

--- src/Datas.hs
+++ src/Datas.hs
@@ -42,12 +42,12 @@ data Data = Data {
 } deriving (Show)
 
 data DuplicateData = DuplicateData {
-    duplicate_image_id      :: Int    ,
-    duplicate_created_at    :: UTCTime,
-    duplicate_updated_at    :: UTCTime,
-    duplicate_first_seen_at :: UTCTime,
-    duplicate_of_id         :: Int    ,
-    duplicate_uploader_id   :: Maybe Int
+    duplicate_image_id      :: Int      ,
+    duplicate_created_at    :: UTCTime  ,
+    duplicate_updated_at    :: UTCTime  ,
+    duplicate_first_seen_at :: UTCTime  ,
+    duplicate_uploader_id   :: Maybe Int,
+    duplicate_of_id         :: Int      
 } deriving (Show)
 
 data Tag = Tag {
@@ -64,7 +64,7 @@ data Comment = Comment {
     comment_id        :: CommentId,
     comment_posted_at :: UTCTime  ,
     comment_image     :: ImageId  ,
-    comment_user      :: Username ,
+    comment_author    :: Username ,
     comment_body      :: String   ,
     comment_deleted   :: Bool
 } | NullComment deriving (Show)
@@ -105,144 +105,99 @@ instance FromJSON ImageData where
             Right imageDuplicate -> return $ ImageDuplicateData imageDuplicate
 
 instance FromJSON DuplicateData where
-    parseJSON = withObject "duplicatedata" $ \o -> do
-        i             <- o .:  "id"
-        created_at    <- o .:  "created_at"
-        updated_at    <- o .:  "updated_at"
-        first_seen_at <- o .:  "first_seen_at"
-        uploader      <- o .:? "uploader_id"
-        duplicate_of  <- o .:  "duplicate_of"
-        return DuplicateData {
-            duplicate_image_id      = i,
-            duplicate_created_at    = created_at,
-            duplicate_updated_at    = updated_at,
-            duplicate_first_seen_at = first_seen_at,
-            duplicate_of_id         = duplicate_of,
-            duplicate_uploader_id   = uploader
-        }
+    parseJSON (Object o) =
+        DuplicateData
+            <$> o .:  "id"
+            <*> o .:  "created_at"
+            <*> o .:  "updated_at"
+            <*> o .:  "first_seen_at"
+            <*> o .:? "uploader_id"
+            <*> o .:  "duplicate_of"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON Data where
-    parseJSON = withObject "data" $ \o -> do
-        i             <- o .:  "id"
-        upvotes       <- o .:  "upvotes"
-        downvotes     <- o .:  "downvotes"
-        faves         <- o .:  "faves"
-        score         <- o .:  "score"
-        height        <- o .:  "height"
-        width         <- o .:  "width"
-        aspect_ratio  <- o .:  "aspect_ratio"
-        description   <- o .:  "description"
-        created_at    <- o .:  "created_at"
-        updated_at    <- o .:  "updated_at"
-        first_seen_at <- o .:  "first_seen_at"
-        tags          <- o .:  "tag_ids"
-        uploader      <- o .:? "uploader_id"
-        comment_count <- o .:  "comment_count"
-        return Data {
-            image_id            = i,
-            image_upvotes       = upvotes,
-            image_downvotes     = downvotes,
-            image_faves         = faves,
-            image_score         = score,
-            image_height        = height,
-            image_width         = width,
-            image_aspect_ratio  = aspect_ratio,
-            image_description   = description,
-            image_created_at    = created_at,
-            image_updated_at    = updated_at,
-            image_first_seen_at = first_seen_at,
-            image_tags          = tags,
-            image_uploader      = uploader,
-            image_comment_count = comment_count
-        }
+    parseJSON (Object o) =
+        Data
+            <$> o .:  "id"
+            <*> o .:  "upvotes"
+            <*> o .:  "downvotes"
+            <*> o .:  "faves"
+            <*> o .:  "score"
+            <*> o .:  "height"
+            <*> o .:  "width"
+            <*> o .:  "aspect_ratio"
+            <*> o .:  "description"
+            <*> o .:  "created_at"
+            <*> o .:  "updated_at"
+            <*> o .:  "first_seen_at"
+            <*> o .:  "tag_ids"
+            <*> o .:? "uploader_id"
+            <*> o .:  "comment_count"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON SearchPage where
-    parseJSON = withObject "searchpage" $ \o -> do
-        search       <- o .: "search"
-        total_count  <- o .: "total"
-        return $ SearchPage total_count search
+    parseJSON (Object o) =
+        SearchPage
+            <$> o .: "search"
+            <*> o .: "total"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON CommentPage where
-    parseJSON = withObject "commentspage" $ \o -> do
-        comments <- o .: "comments"
-        return $ CommentPage comments
+    parseJSON (Object o) =
+        CommentPage <$> o .: "comments"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON Comment where
-    parseJSON = withObject "comment" $ \o -> do
-        i         <- o .: "id"
-        body      <- o .: "body"
-        author    <- o .: "author"
-        image     <- o .: "image_id"
-        posted_at <- o .: "posted_at"
-        deleted   <- o .: "deleted"
-        return Comment {
-            comment_id        = i,
-            comment_body      = body,
-            comment_user      = author,
-            comment_image     = image,
-            comment_posted_at = posted_at,
-            comment_deleted   = deleted
-        }
+    parseJSON (Object o) =
+        Comment
+            <$> o .: "id"
+            <*> o .: "posted_at"
+            <*> o .: "image_id"
+            <*> o .: "author"
+            <*> o .: "body"
+            <*> o .: "deleted"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON User where
-    parseJSON = withObject "user" $ \o -> do
-        awards        <- o .:  "awards"
-        i             <- o .:  "id"
-        name          <- o .:  "name"
-        role          <- o .:  "role"
-        description   <- o .:? "description"
-        created_at    <- o .:  "created_at"
-        comment_count <- o .:  "comment_count"
-        upload_count  <- o .:  "uploads_count"
-        return $ User {
-            user_id            = i,
-            user_name          = name,
-            user_description   = description,
-            user_role          = role,
-            user_created_at    = created_at,
-            user_upload_count  = upload_count,
-            user_comment_count = comment_count,
-            user_awards        = awards
-        }
+    parseJSON (Object o) =
+        User
+            <$> o .:  "id"
+            <*> o .:  "role"
+            <*> o .:  "created_at"
+            <*> o .:  "comment_count"
+            <*> o .:  "uploads_count"
+            <*> o .:  "awards"
+            <*> o .:  "name"
+            <*> o .:? "description"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON Award where
-    parseJSON = withObject "award" $ \o -> do
-        i          <- o .: "id"
-        title      <- o .: "title"
-        awarded_on <- o .: "awarded_on"
-        label      <- o .: "label"
-        return $ Award {
-            award_id    = i,
-            award_title = title,
-            award_label = label,
-            award_date  = awarded_on
-        }
+    parseJSON (Object o) =
+        Award
+            <$> o .: "id"
+            <*> o .: "title"
+            <*> o .: "label"
+            <*> o .: "awarded_on"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON Tag where
-    parseJSON = withObject "tag" $ \o -> do
-        i                 <- o .:  "id"
-        name              <- o .:  "name"
-        slug              <- o .:  "slug"
-        description       <- o .:  "description"
-        short_description <- o .:  "short_description"
-        aliased_to        <- o .:? "aliased_to_id"
-        implied_tags      <- o .:  "implied_tag_ids"
-        return $ Tag {
-            tag_id                = i,
-            tag_name              = name,
-            tag_slug              = slug,
-            tag_description       = description,
-            tag_short_desctiption = short_description,
-            tag_aliased_to        = aliased_to,
-            tag_implied_tags      = implied_tags
-        }
+    parseJSON (Object o) =
+        Tag
+            <$> o .:  "id"
+            <*> o .:  "name"
+            <*> o .:  "slug"
+            <*> o .:  "short_description"
+            <*> o .:  "description"
+            <*> o .:  "implied_tag_ids"
+            <*> o .:? "aliased_to_id"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance FromJSON Settings where
     parseJSON (Object v) = 
         Settings
             <$> v .: "key"
             <*> v .: "images_per_page"
-    parseJSON _ = fail "Unable to parse database credentials"
+    parseJSON _          = fail "Unable to parse non-Object"
 
 instance Nullable ImageData where
     null = ImageNullData
